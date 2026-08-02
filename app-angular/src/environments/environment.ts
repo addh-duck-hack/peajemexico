8aeb516133f017f756aab1f4d6755244b9b8dbaa
addh-duck-hack <159945819+addh-duck-hack@users.noreply.github.com>
2026-08-02 17:30:54 -0600

    Delete app-angular/src/environments/environment.ts

--- app-angular/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  companyName: 'PeajesMX',
-  companySlogan: '¿Planeas un viaje por carretera en México? Con PeajesMX.com puedes calcular fácilmente el costo total de casetas en rutas con múltiples destinos.',
-  urlbackend: 'https://api.peajesmx.com',
-  localStorageName: 'peaje_local_storage',
-  gaMeasurementId: 'G-FES9SQVGK9',
-  adsensePublisherId: 'ca-pub-1652780964097780'
-};
